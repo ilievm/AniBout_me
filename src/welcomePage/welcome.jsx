ec3a43f4870baf83e6004b93583af34ffee60b4f
unknown <mikluazshnorkel@gmail.com>
2020-09-29 10:57:36 -0400

    moved functions around and updated css. Saving before moving further

--- src/welcomePage/welcome.jsx
+++ src/welcomePage/welcome.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import './welcome.css'
 
 class Welcome extends Component {
   render() {
     return (
-      <div>
-        ШКЯЪ
-        <button id="submit">Search</button>
+      <div className="container">
+        <h1></h1>
+        <h1 class="c-article__title">
+          INTELLIGENCE OF AI FOR YOU        
+        </h1>
+        <p className="welcome-p">use the power of image recognition to find gifs</p>
+        <h3 className="welcome-h3">To get started just upload an image or insert image URL</h3>
       </div>
     );
   }
